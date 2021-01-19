79435e13f0d7a0874dd405bf81b696ef8f2be6a7
Кирилл Алдашкин <a89086520092k@yandex.ru>
2021-01-20 00:24:54 +0800

    Changing the structure

--- setup.bat
+++ setup.bat
@@ -1,6 +1,3 @@
 @rmdir /S /Q "%appdata%\..\..\Documents\Arduino\libraries\ElectricalAge-Arduino-Library"
 @mkdir "%appdata%\..\..\Documents\Arduino\libraries\ElectricalAge-Arduino-Library"
-@copy /B ElnCommunicator.cpp /B "%appdata%\..\..\Documents\Arduino\libraries\ElectricalAge-Arduino-Library\" /B
-@copy /B ElnCommunicator.h /B "%appdata%\..\..\Documents\Arduino\libraries\ElectricalAge-Arduino-Library\" /B
-@copy /B keywords.txt /B "%appdata%\..\..\Documents\Arduino\libraries\ElectricalAge-Arduino-Library\" /B
-@xcopy examples "%appdata%\..\..\Documents\Arduino\libraries\ElectricalAge-Arduino-Library\examples\" /E
+@xcopy library "%appdata%\..\..\Documents\Arduino\libraries\ElectricalAge-Arduino-Library\" /E
