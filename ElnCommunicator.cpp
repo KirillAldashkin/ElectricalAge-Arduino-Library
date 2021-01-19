09c44281fc378d43f5a6e68be4c39b14e00e5b94
Кирилл Алдашкин <a89086520092k@yandex.ru>
2021-01-19 12:55:48 +0800

    Added numerical channels

--- ElnCommunicator.cpp
+++ ElnCommunicator.cpp
@@ -5,8 +5,14 @@
 #define readOneDigit (Serial.read() - 48)
 #define readTwoDigits (readOneDigit * 10 + readOneDigit)
 #define readThreeDigits (readTwoDigits * 10 + readOneDigit)
+#define readFourDigits (readThreeDigits * 10 + readOneDigit)
+#define readFiveDigits (readFourDigits * 10 + readOneDigit)
+
 void ElnCommunicator::OpenConnection() {
-    Serial.begin(115200);
+    #if DEBUG
+		Serial.println("Wait connection request");
+	#endif
+	Serial.begin(115200);
 	// Clearing the serial port
 	char reading;
 	do {
@@ -18,10 +24,16 @@ void ElnCommunicator::OpenConnection() {
 	char a = Serial.read();
 	char b = Serial.read();
 	while(true) {
+		#if DEBUG
+			Serial.println("Wait two symbols");
+		#endif
 		while(Serial.available() > 0) {
 			a = b;	
 			b = Serial.read();
 		}
+		#if DEBUG
+			Serial.print("Symbols are: "); Serial.print(a); Serial.println(b);
+		#endif
 		if(a == 'C' && b == '\n') break;
 	}
 	// Sending the type of board
@@ -71,6 +83,12 @@ void ElnCommunicator::WaitMessage() {
 				Serial.println("Commands to read pin values");
 			#endif
 			readMessageReadPins();
+		} else if(header == 'N' || header == 'n') {
+			// Commands to read/write numerical channels
+			#if DEBUG
+				Serial.println("Commands to read/write numerical channels");
+			#endif
+			editChannels();
 		} else {
 			// Cleaning out the trash
 			#if DEBUG
@@ -166,6 +184,8 @@ void ElnCommunicator::readMessageWritePins() {
 			// PWM Write
 			while(Serial.available() < 3);
 			write_value = readThreeDigits;
+			if(write_value > 255)
+				write_value = 255;
 			#if DEBUG
 				Serial.print("    PWM Write: "); Serial.println(write_value);
 			#endif
@@ -214,12 +234,79 @@ void ElnCommunicator::readMessageReadPins() {
 		#endif
 	}
 }
+void ElnCommunicator::editChannels() {
+	while(Serial.available() < 2);
+	int count = readTwoDigits;
+	#if DEBUG
+		Serial.print("  Count: "); Serial.println(count);
+		Serial.print("  Buffer pointer: "); Serial.println(buffer_pointer);
+	#endif
+	buffer[buffer_pointer++] = 'N';
+	int count_pointer = buffer_pointer; // Memorizing the position to record the number of channels read later
+	buffer_pointer += 2;
+	int readed_count = 0;
+	for(int i = 0; i < count; i++) {
+		while(Serial.available() < 3);
+		int channel_number = readTwoDigits % 32;
+		int mode = readOneDigit;
+		#if DEBUG
+			Serial.print("  "); Serial.print(i); Serial.println(':');
+			Serial.print("    Channel number: "); Serial.println(channel_number);
+			Serial.print("    Mode: "); Serial.println(channel_number);
+		#endif
+		if(mode == 0) {
+			// Reading value
+			readed_count++;
+			#if DEBUG
+				Serial.print("    Reading value "); Serial.println(readed_count);
+			#endif
+			intToBuffer(channel_number, 2);
+			int16_t val = Channels[channel_number];
+			if(val < 0) {
+				val = -val;
+				buffer[buffer_pointer++] = '-';
+				#if DEBUG
+					Serial.println("    Is negative");
+				#endif
+			}
+			intToBuffer(val, 5);
+			#if DEBUG
+				Serial.print("    Absolute value: "); Serial.println(val);
+			#endif
+		} else {
+			// Writing value
+			#if DEBUG
+				Serial.println("    Writing value");
+			#endif
+			bool negative = false;
+			while(Serial.available() == 0);
+			if(Serial.peek() == '-') {
+				Serial.read();
+				negative = true;
+				#if DEBUG
+					Serial.println("    Is negative");
+				#endif
+			}
+			while(Serial.available() < 5);
+			int16_t val = readFiveDigits;
+			#if DEBUG
+				Serial.print("    Absolute value: "); Serial.println(val);
+			#endif
+			if(negative) val = -val;
+			Channels[channel_number] = val;
+		}
+		// Recording the number of channels read
+		int cur_pointer = buffer_pointer;
+		buffer_pointer = count_pointer;
+		intToBuffer(readed_count, 2);
+		buffer_pointer = cur_pointer;
+	}
+}
 void ElnCommunicator::intToBuffer(int value, int length) {
 	// Write this number to the response buffer,
 	// adding leading zeros to the specified length
-	for(int i = 0; i < length; i++) {
+	for(int i = 0; i < length; i++)
 		buffer[buffer_pointer++] = '0';
-	}
 	int count = 0;
 	while(value > 0) {
 		buffer[--buffer_pointer] = (char)(value % 10 + 48);
--- ElnCommunicator.h
+++ ElnCommunicator.h
@@ -6,12 +6,15 @@ class ElnCommunicator {
 		//ElnCommunicator(); // Constructor
 		void WaitMessage(); // Stops the program waiting for a message
 		void OpenConnection(); // Stops the program while waiting for the connection to open
+		int16_t Channels[32] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; // Numerical channels
 	private:
 		void readSettings();
 		void readMessageWritePins();
 		void readMessageReadPins();
+		void editChannels();
 		void intToBuffer(int value, int length);
 		char buffer[512]; // The response buffer
 		int buffer_pointer = 0;
+		
 };
 #endif
