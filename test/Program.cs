9df59ea9d97c2dc54217b08f0c560b18f120cabd
KirillAldashkin <a89086520092k@yandex.ru>
2021-01-21 20:39:59 +0800

    The stress test code no longer makes you want to scream.

--- test/Program.cs
+++ test/Program.cs
@@ -3,33 +3,50 @@ using System.IO.Ports;
 using System.Diagnostics;
 using System.Threading;
 
+/*
+  Stress Test
+  Sends the next packet as soon as the Arduino processes
+  the previous one. Displays information about the speed.
+*/
+
+// COM Port speed
+const int SPEED = 115200;
+// Options for establishing the connection
 const int CONNECT_REQUEST_DELAY = 25;
 const int MAX_CONNECT_REQUESTS = 16;
+// Pin parameters
 int[] DIGITAL_READ = {2, 4, 7, 8, 12, 13};
 int[] ANALOG_READ = {14, 15, 16, 17, 18, 19};
 int[] PWM_WRITE = {3, 5, 6, 9, 10, 11};
 
-// Getting the port number
+
+// Getting the port name
 Console.WriteLine("Enter the COM port number:");
 Console.WriteLine("Note - available COM ports:");
 foreach(string c_port in SerialPort.GetPortNames())
     Console.WriteLine($"  {c_port}");
 Console.Write(">");
 string port_name = Console.ReadLine();
-// Establishing a connection
+
+// Opening the COM port
 Console.WriteLine($"The {port_name} port is selected.");
-Stopwatch stp = new Stopwatch();
 SerialPort port = new SerialPort();
 Console.Write("Opening the port... ");
 try {
-    port = new SerialPort((string)port_name, 115200);
+    port = new SerialPort((string)port_name, SPEED);
     port.NewLine = "\n";
     port.Open();
 } catch(Exception e) {
     Console.WriteLine();
-    Exit(e.ToString(), 1);
+    #if DEBUG
+        Exit(e.ToString(), 1);
+    #else
+        Exit("Error when opening the COM port.", 1);
+    #endif
 }
 Console.WriteLine($"Done.");
+
+// Establishing a connection
 Console.Write("Establishing a connection... Request ");
 int r = 1;
 int cl = Console.CursorLeft;
@@ -44,17 +61,21 @@ if(r == MAX_CONNECT_REQUESTS+1)
 string board_type = port.ReadLine().Substring(1).TrimEnd('_');
 Console.WriteLine();
 Console.WriteLine($"The connection is established. Board Type: {board_type}");
+
 // Stress Test
 Console.CursorTop++;
+string packet;
+Stopwatch stp = new Stopwatch();
 while(true) {
+    packet = GeneratePacket();
     stp.Reset();
     stp.Start();
-    port.WriteLine(GenPacket());
+    port.WriteLine(packet);
     string res = port.ReadLine(); 
     stp.Stop();
     Console.CursorTop--;
     long freq = 1000 / stp.ElapsedMilliseconds;
-    Console.WriteLine($"Request timeout: {stp.ElapsedMilliseconds} ms. Estimated frequency: {freq} packet{((freq!=1)?"s":"")} per second.");
+    Console.WriteLine($"Request timeout: {stp.ElapsedMilliseconds} ms. Estimated frequency: {freq} packet{((freq!=1)?"s":"")} per second. ");
     Console.WriteLine(new string(' ', Console.WindowWidth - Console.CursorLeft));
     Console.CursorTop--;
 }
@@ -73,7 +94,9 @@ void Exit(string e = "", int code = 0) {
     Console.ReadKey(true);
     Environment.Exit(code);
 }
-string GenPacket() {
+// Generates a packet that randomly changes the numeric
+// channels and operates with the specified pins.
+string GeneratePacket() {
     string packet = "N32";
     Random rnd = new Random();
     for(int i = 0; i < 32; i++) {
