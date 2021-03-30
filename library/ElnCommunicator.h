e70cacd1042d143be180cec5b653f4594d8c4771
KirillAldashkin <a89086520092k@yandex.ru>
2021-03-30 20:36:23 +0800

    Added descriptions of public methods and fields (including for displaying them in the Arduino IDE 2.0).

--- library/ElnCommunicator.h
+++ library/ElnCommunicator.h
@@ -3,17 +3,41 @@
 #include <Arduino.h>
 #define DEBUG false
 #define SPEED 500000
+// Provides the ability to communicate between the Arduino board and the ElectricalAge mod.
 class ElnCommunicator {
 	public:
+		// If the connection was established, returns true, otherwise returns false.
 		bool IsConnected();
+		/* 
+		  If the input buffer is not empty, the message is read and processed from it,
+		  and the method returns true. If the input buffer is empty, returns false.
+		*/
 		bool CheckMessage();
-		void WaitMessage(); // Stops the program waiting for a message
+		// Waits for a message, then processes it.
+		void WaitMessage();
+		/*
+		  If the connection is already established, it does nothing and returns true.
+		  Otherwise, if there is data in the input buffer, the connection request is
+		  read from it and processed, and method returns true. If the input buffer is
+		  empty, method returns false.
+		*/
 		bool TryOpenConnection(); 
-		void WaitOpenConnection(); // Stops the program while waiting for the connection to open
-		void LockPin(int pin); // Blocks the pin from being changed from the outside
-		void UnlockPin(int pin); // Unblocks the pin
-		bool IsLocked(int pin); // Determines whether the pin is blocked
-		int16_t Channels[32] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; // Numerical channels
+		/*
+		  If the connection is already established, does nothing, otherwise waits for
+		  the connection request and processes it.
+		*/
+		void WaitOpenConnection();
+		// Blocks the pin, forbidding any operations on it from the PC.
+		void LockPin(int pin);
+		// Unlocks the pin, allowing operations on it from the PC.
+		void UnlockPin(int pin);
+		// If the pin is blocked, returns true, otherwise returns false.
+		bool IsLocked(int pin);
+		/*
+		  Provides 32 [0...31] numeric channels that store integers from -32768 to 32767.
+		  Can be changed both by the PC and Arduino.
+		*/
+		int16_t Channels[32] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 	private:
 		bool connected = false;
 		void changePinModes();
